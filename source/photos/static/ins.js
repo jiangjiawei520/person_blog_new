938e1cb56742b642eab41b52f92316aafa5219c9
jiangjiawei <2314857815@qq.com>
2026-03-16 11:42:48 +0800

    Back up my blog

--- source/photos/static/ins.js
+++ source/photos/static/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://ghproxy.net/https://raw.githubusercontent.com/jiangjiawei520/person_blog_new/master/photos/min_photos/' + data.link[i];
-          var src = 'https://ghproxy.net/https://raw.githubusercontent.com/jiangjiawei520/person_blog_new/master/photos/min_photos/' + data.link[i];
+          var minSrc = 'https://gh-proxy.jiangjiawei.eu.cc/https://raw.githubusercontent.com/jiangjiawei520/person_blog_new/master/photos/min_photos/' + data.link[i];
+          var src = 'https://gh-proxy.jiangjiawei.eu.cc/https://raw.githubusercontent.com/jiangjiawei520/person_blog_new/master/photos/min_photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
